e77027f3494a2e372d77406aafb8d2755625b755
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-22 06:22:17 -0700

    just putchar function

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*_putchar - Function printing character
+*@buffersize: String
+*@buffer: String
+*Return: Integer
+*/
+
+int _putchar(char *buffer, int buffersize)
+{
+	return (write(1, buffer, buffersize));
+}
