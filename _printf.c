e93ec2cd59ad6e82d53a5c7d30a5007b5fed19c7
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-22 21:02:27 -0700

    some changes function

--- _printf.c
+++ _printf.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _printf - Custom printf function
-* @format: Format string with conversion specifiers
-* Return: Number of characters printed
-*/
+ * _printf - Custom printf function
+ * @format: Format string with conversion specifiers
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -23,39 +23,26 @@ int _printf(const char *format, ...)
 		if (*format != '%')
 		{
 			if (buffer_size == 1024)
-			{
-				_putchar(buffer, buffer_size);
-				buffer_size = 0;
-			}
-			buffer[buffer_size++] = *format;
-			printed_chars++;
+				_putchar(buffer, buffer_size), buffer_size = 0;
+			buffer[buffer_size++] = *format, printed_chars++;
 		}
 		else if (*(format + 1) != '\0')
 		{
-			format++;
-			handler = get_handler(*format);
+			format++, handler = get_handler(*format);
 			if (handler)
-			{
 				buffer_size = handler(args, buffer, buffer_size);
-			}
 			else
 			{
 				if (buffer_size == 1024)
-				{
-					_putchar(buffer, buffer_size);
-					buffer_size = 0;
-				}
-				buffer[buffer_size++] = '%';
-				buffer[buffer_size++] = *format;
+					_putchar(buffer, buffer_size), buffer_size = 0;
+				buffer[buffer_size++] = '%', buffer[buffer_size++] = *format;
 				printed_chars += 2;
 			}
 		}
 		format++;
 	}
-
 	if (buffer_size > 0)
 		_putchar(buffer, buffer_size);
-
 	va_end(args);
 	return (printed_chars);
 }
